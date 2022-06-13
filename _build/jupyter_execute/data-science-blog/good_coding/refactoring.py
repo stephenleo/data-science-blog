948445b56830b0476a8ac62313114b98684403f3
stephenleo <stephen.leo87@gmail.com>
2022-06-13 16:32:37 +0800

    adding sourcery

---
+++ _build/jupyter_execute/data-science-blog/good_coding/refactoring.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 🔩 Refactoring
+
+# ## 🧹 Sourcery: Automated code refactoring!
+# 
+# - Real-time refactoring suggestions with one-click updates
+# - Code quality feedback
+# - Refactoring explanations to make you a better programmer!
+# - Star the repo to get an automated refactoring PR on your most popular repos!
+# - VSCode Extension
+# 
+# 🌟 Github: [https://github.com/sourcery-ai/sourcery](Sourcery)
+# 
+# 🔥 VSCode/ Pycharm extensions: [https://sourcery.ai/](https://sourcery.ai/)
+# 
+# 💯 Supported Refactorings: [https://docs.sourcery.ai/refactorings/](https://docs.sourcery.ai/refactorings/)
+# 
+# #datascience #dataanalytics #coding #github #artificialintelligence
+# 
+# <video src="images/refactoring/sourcery.mp4"></video>
+
+# In[ ]:
+
+
+
+
